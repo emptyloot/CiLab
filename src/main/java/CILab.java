a7932c979721ce2368ced9277740c2c103092791
David Caldwell <daviddcmmoo@gmail.com>
2022-06-13 22:23:32 -0700

    add logic to detectCaseUse

--- src/main/java/CILab.java
+++ src/main/java/CILab.java
@@ -2,6 +2,12 @@
 
 public class CILab implements CILabInterface {
     private String myString;
+    
+    //Constructors
+    
+    public CILab() {
+    	myString = null;
+    }
 
     @Override
     public String getString() {
@@ -15,7 +21,20 @@ public class CILab implements CILabInterface {
 
     @Override
     public boolean detectCapitalUse() {
+    	//Test lower case and upper case strings
+    	if (myString.equals(myString.toUpperCase()))
+    		return true;
+    	if (isLowerCase(myString))
+    		return true;
+    	// If first letter is capital check the tail for lower case
+    	if (myString.charAt(0)> 64 && myString.charAt(0) <  96 )
+    		if (isLowerCase(myString.substring(1)))
+    			return true;
         return false;
+    }
+    
+    private boolean isLowerCase(String text) {
+    	return text.equals(myString.toLowerCase());
     }
 
 }
